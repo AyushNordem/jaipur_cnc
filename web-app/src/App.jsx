2f338cefc3a602b05a56be76824bfdc80cb673b9
ayushnorderm <ayushnordem@gmail.com>
2026-07-28 12:50:16 +0530

    fix issue

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -5,12 +5,14 @@ import Gallery from './pages/Gallery';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
 import About from './pages/About';
+import ScrollToTop from './components/ScrollToTop';
 import { SiteProvider } from './context/SiteContext';
 
 function App() {
   return (
     <SiteProvider>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
